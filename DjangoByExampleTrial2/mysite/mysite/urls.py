d99b7c32f2d3169dd94c657a709e0d6c6b486940
Kaushiksekar <kaushik3993@gmail.com>
2018-11-18 21:02:15 +0530

    Commit for adding a sitemap to the site

--- DjangoByExampleTrial2/mysite/mysite/urls.py
+++ DjangoByExampleTrial2/mysite/mysite/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'blog/', include('blog.urls')),
+    url(r'sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
