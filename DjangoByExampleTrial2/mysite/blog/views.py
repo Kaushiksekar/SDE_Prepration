1427aea5cd4b8ea16e24a7b8080b384f21d3bd85
Kaushiksekar <kaushik3993@gmail.com>
2018-11-18 20:26:33 +0530

    Updated commit for last commit- Chapter 2

--- DjangoByExampleTrial2/mysite/blog/views.py
+++ DjangoByExampleTrial2/mysite/blog/views.py
@@ -29,14 +29,8 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
 
     post_tags_ids = post.tags.values_list('id', flat=True)
-    print('post_tags_ids')
-    print(post_tags_ids)
     similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    print('similar_posts')
-    print(similar_posts)
     similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
-    print('similar_posts')
-    print(similar_posts)
 
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
